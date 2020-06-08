5dbae562b3f34cc8fcf59f9a39a1966c3eb9bbd7
iamtaegu <gmdals45@gmail.com>
2020-06-08 16:12:07 +0900

    VueJs

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http'); 
+var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈
+
+
+function onRequest(request, response){ 
+
+    if(request.method == 'GET' && request.url == '/'){ 
+        response.writeHead(200,{"Content-Type":"text/html"}); 
+        fs.createReadStream("./countup.html").pipe(response); 
+    } 
+}
+
+http.createServer(onRequest).listen(8888);
